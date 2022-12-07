4c06a1c2ef5b5f5902500c0a0f1bb1105a18a7dd
Ilaria Pilo <ila.pilo99@gmail.com>
2022-12-07 13:50:58 +0100

    [Preprocessing] Add k-fold

--- preprocessing.py
+++ preprocessing.py
@@ -6,66 +6,92 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
 
-def preprocessing(dataset_file, split):
+def load_dataset(dataset_file):
     """
-    Prepares the dataset, applying different kinds of data preprocessing.
+    Reads the dataset from file, extracting the label column.
 
     
     Parameters
     ----------
     dataset_file: str
         The name of the file containing the dataset (.pkl)
-    split: dict
-        The set of parameters regarding the dataset split.
-        It should contain the following keys:
-        - 'test': float - the fraction of the dataset we want to keep as test set
-        - 'stratify': bool - whether we want the split to keep the same proportion between classes as
-           the original dataset
-        - 'k': int - the number of folds for the k-fold validation approach
-
-
-
-
-    # FIXME - just some template to follow for later
-    print_cols : bool, optional
-        A flag used to print the columns to the console (default is False)
 
     Returns
     -------
-    list
-        a list of strings representing the header columns
-
-
-
+    X
+        DataFrame containing the dataset samples
+    y
+        Column (FIXME?) containing the correspondent labels
     """
+
     # first, load the dataframe
     X = pd.read_pickle(dataset_file)
 
     # extract the label column, and remove it from the original dataframe
     y = X['match']
     X.drop('match', inplace=True)
+
+    return X, y
+
+
+
+def split_dataset(X, y, test=0.3, stratify=True, k=5):
+    """
+    Splits the dataset into training and test set, then create KFold model.
+
+    
+    Parameters
+    ----------
+    X: DataFrame
+        The dataset samples
+    y: FIXME
+        The corresponding labels
+    test: float, optional (default is 0.3)
+        The fraction of the dataset we want to keep as test set
+    stratify: bool, optional (default is True)
+        Whether we want the split to keep the same proportion between classes as the original dataset
+    k: int, optional (default is 5)
+        The number of folds for the k-fold validation approach
+
+    Returns
+    -------
+    X_tr, y_tr
+        Samples and relative labels for the training set
+    X_te, y_te
+        Samples and relative labels for the test set
+    kf
+        KFold model (to be applied on X_tr, y_tr)
+
+    """
     
     # split it into train and test set
-    stratify = (y if split.stratify else None)
-    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=split.test, stratify=stratify)
+    stratify = (y if stratify else None)
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=test, stratify=stratify)
+
+    # create the k-fold class
+    kf = KFold(n_splits=k, shuffle=True)
+
+    return X_tr, y_tr, X_te, y_te, kf
 
 
 
-    
+#########################################
+#                                       #
+#             USAGE EXAMPLE             #
+#                                       #
+#########################################    
+
+if __name__ == "__main__":
+
+    # first, we load the dataset
+    X, y = load_dataset('./data/data.pkl')
+    # then, we split it
+    X_tr, y_tr, X_te, y_te, kf = split_dataset(X, y)
 
 
 
 
-
-
-split = {
-    'test': 0.3,
-    'stratify': True,
-    'k': 10
-}
-
-
-preprocessing('data/data.pkl')
 
