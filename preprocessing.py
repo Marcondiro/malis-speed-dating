d28563c424114b346af9a4540865eb4e79d4d5f5
Ilaria Pilo <ila.pilo99@gmail.com>
2022-12-07 12:08:10 +0100

    [Preprocessing] Add train-test-split

--- preprocessing.py
+++ preprocessing.py
@@ -5,8 +5,10 @@
 ##########################################
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
-def preprocessing(dataset_file):
+
+def preprocessing(dataset_file, split):
     """
     Prepares the dataset, applying different kinds of data preprocessing.
 
@@ -15,6 +17,13 @@ def preprocessing(dataset_file):
     ----------
     dataset_file: str
         The name of the file containing the dataset (.pkl)
+    split: dict
+        The set of parameters regarding the dataset split.
+        It should contain the following keys:
+        - 'test': float - the fraction of the dataset we want to keep as test set
+        - 'stratify': bool - whether we want the split to keep the same proportion between classes as
+           the original dataset
+        - 'k': int - the number of folds for the k-fold validation approach
 
 
 
@@ -32,21 +41,30 @@ def preprocessing(dataset_file):
 
     """
     # first, load the dataframe
-    df = pd.read_pickle(dataset_file)
+    X = pd.read_pickle(dataset_file)
 
-    # We apply OneHotEncoding
-    # Notice that the only attributes that require OneHotEncoding are those which are:
-    #   - categorical AND
-    #   - their value does not match an actual scale of importance
-    # field_cd, race, goal, career_c
-    df = pd.get_dummies(data=df, columns=['field_cd_x', 'race_x', 'goal_x', 'career_c_x', 'field_cd_y', 'race_y', 'goal_y', 'career_c_y'])
-
-
-
+    # extract the label column, and remove it from the original dataframe
+    y = X['match']
+    X.drop('match', inplace=True)
+    
+    # split it into train and test set
+    stratify = (y if split.stratify else None)
+    X_tr, X_te, y_tr, y_te = train_test_split(X, y, test_size=split.test, stratify=stratify)
 
 
 
+    
 
+
+
+
+
+
+split = {
+    'test': 0.3,
+    'stratify': True,
+    'k': 10
+}
 
 
 preprocessing('data/data.pkl')
